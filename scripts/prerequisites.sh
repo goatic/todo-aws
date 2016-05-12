96f113f8a0176fbdcbaa9e8dec32c8c4a60fd081
w0ns88 <dotdjw@gmail.com>
2016-05-12 18:32:48 +0200

    prerequisites -y flag added

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -5,4 +5,4 @@ sudo yum check-update
 #NodeJS 6.0.0
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
 sudo yum -y install nodejs
-sudo yum groupinstall 'Development Tools'
+sudo yum groupinstall -y 'Development Tools'
