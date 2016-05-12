ea2db702f0a24c75f90fa1a638f036365acd80d0
w0ns88 <dotdjw@gmail.com>
2016-05-12 18:30:27 +0200

    prerequisites script sudo added

--- scripts/prerequisites.sh
+++ scripts/prerequisites.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-yum check-update
+sudo yum check-update
 
 #NodeJS 6.0.0
 curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum -y install nodejs
-yum groupinstall 'Development Tools'
+sudo yum -y install nodejs
+sudo yum groupinstall 'Development Tools'
